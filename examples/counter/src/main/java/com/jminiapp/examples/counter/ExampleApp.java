8892588bda48f0f1bc5be46599207850d7d35d91
AlonPal09 <143242728+AlonPal09@users.noreply.github.com>
2025-12-05 01:02:57 -0600

    feat: add mini example app

---
+++ examples/counter/src/main/java/com/jminiapp/examples/counter/ExampleApp.java
@@ -0,0 +1,8 @@
+package com.jminiapp.examples.counter;
+
+public class ExampleApp {
+    public static void main(String[] args) {
+        System.out.println("Hello from my mini application!");
+        System.out.println("Use CounterAppRunner to run the counter application.");
+    }
+}
